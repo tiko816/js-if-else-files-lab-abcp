e57ee9470a73b8a1db42843f8b2375b5f42461eb
Joseph Carter <jdc16@logicalintent.com>
2018-01-17 22:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var height = "70";
 if (height === 70) {
     console.log("The height is correct") ;
 } else {
-    console.log("You are a Butt") ;
+    console.log("expcted 70") ;
 }
 // Don't worry about this
 module.exports = { name, height
